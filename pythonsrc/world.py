8c58625b3812d4c854d3cafd3184a03b06c46e05
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-03 14:03:42 -0700

    added more functions

--- pythonsrc/world.py
+++ pythonsrc/world.py
@@ -7,3 +7,29 @@ class world:
 
     def getCoordinate(i, limb_idx):
         pass
+
+    def getM1(i, limb_idx):
+        pass
+
+    def getM2(i, limb_idx):
+        pass
+
+    def getCurrentTime():
+        pass
+
+    def simulationRuning():
+        pass
+
+    def getTimeStep():
+        pass
+
+    def printSimData():
+        pass
+
+    def floorExists():
+        pass
+
+    def getFloorZ():
+        pass
+
+    
