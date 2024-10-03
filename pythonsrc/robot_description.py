d93647961cf5f5d19173b277b8bccbb0db958908
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-03 13:57:58 -0700

    added robot description file

---
+++ pythonsrc/robot_description.py
@@ -0,0 +1,10 @@
+
+def get_robot_description(argc, 
+                          argv,
+                          soft_robots,
+                          forces,
+                          logger,
+                          sim_params,
+                          renderParams
+                          ):
+    pass
