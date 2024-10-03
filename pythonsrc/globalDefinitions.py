a6fb269165a046602b44639a5e2999c9e7faaa1c
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-03 10:50:31 -0700

    updating global definitions

--- pythonsrc/globalDefinitions.py
+++ pythonsrc/globalDefinitions.py
@@ -1,2 +1,9 @@
 from enum import Enum
+import numpy as np
+
+class IntegratorMethod(Enum):
+    FORWARD_EULER = "FORWARD_EULER"
+    VERLET_POSITION = "VERLET_POSITION"
+    BACKWARD_EULER = "BACKWARD_EULER"
+    IMPLICIT_MIDPOINT = "IMPLICIT_MIDPOINT"
 
