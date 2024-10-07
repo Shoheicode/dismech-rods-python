a68dd5a3ccb9b614b565c15273b619726c29eaeb
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-07 14:43:16 -0700

    added comment

--- pythonsrc/main.py
+++ pythonsrc/main.py
@@ -1,9 +1,42 @@
-print("How are you?")
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
-def main():
-    print("What about you?")
+# Note THIS ONLY WORKS IN PYOPENGL 3.1.0
 
-print("I am fine")
+# pip install PyOpenGL==3.1.0 use command to download it
 
-if __name__ == "__main__":
-    main()
+#MAKES A SQUARE
+w,h= 500,500
+def square():
+    glBegin(GL_QUADS)
+    glVertex2f(100, 100)
+    glVertex2f(200, 100)
+    glVertex2f(200, 200)
+    glVertex2f(100, 200)
+    glEnd()
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)
+    square()
+    glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow("OpenGL Coding Practice")
+glutDisplayFunc(showScreen)
+glutIdleFunc(showScreen)
+glutMainLoop()
