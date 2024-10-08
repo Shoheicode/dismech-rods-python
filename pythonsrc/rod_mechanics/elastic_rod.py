20f36ca70d7e8d1fd37b0b0c3db18adeba113213
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-08 11:59:24 -0700

    updated elastic rod

---
+++ pythonsrc/rod_mechanics/elastic_rod.py
@@ -0,0 +1,15 @@
+class elasticRod():
+    def __init__(self):
+        pass
+
+    def __init__(self, hi):
+        pass
+
+    def prepareForIteration():
+        pass
+
+    def updateNewtonX():
+        pass
+
+    def updateGuess(weight, dt):
+        pass
