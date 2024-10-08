23fa844afad9ff10c7f5120f7c4a7f35fe4d2e47
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-08 13:42:52 -0700

    updated simulation code

---
+++ pythonsrc/simulation_environment/openglDERSimulationEnvironment.py
@@ -0,0 +1,14 @@
+global world
+world = None
+
+global opengl_logger
+opengl_logger = None
+
+global opengl_is_logging 
+opengl_is_logging= False
+
+opengl_cmdline_per = 0
+
+class openglDERSimulationEnvironment():
+    def __init__(self):
+        pass
