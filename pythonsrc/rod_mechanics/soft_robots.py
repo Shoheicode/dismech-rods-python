67bc1fe919988c22622d164e37ded2f506ecfd84
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-08 11:47:57 -0700

    added softrobots library

---
+++ pythonsrc/rod_mechanics/soft_robots.py
@@ -0,0 +1,11 @@
+class softRobots():
+    def __init__(self):
+        pass
+
+    def addLimb(start, end, num_nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
+        pass
+
+    def addLimb(nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
+        pass
+
+    
