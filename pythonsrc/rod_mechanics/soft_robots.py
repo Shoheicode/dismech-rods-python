f87fc94eacd766a6f8a47faacf0737fa54762750
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-08 11:51:36 -0700

    added a softrobot.h library code

--- pythonsrc/rod_mechanics/soft_robots.py
+++ pythonsrc/rod_mechanics/soft_robots.py
@@ -8,4 +8,23 @@ class softRobots():
     def addLimb(nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
         pass
 
+    def createJoint(limb_idx, node_idx):
+        pass
+
+    def addToJoint(joint_idx, limb_idx, node_idx):
+        pass
+
+    def lockEdge(limb_idx, edge_idx):
+        pass
+
+    def applyInitialVelocities(limb_idx, velocities):
+        pass
+
+    def setup():
+        pass
+
+    # void addController(const shared_ptr<baseController>& controller);
+    def addController(controller):
+        pass
+
     
