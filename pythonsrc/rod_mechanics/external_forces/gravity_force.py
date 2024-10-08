9a2ef9085cfd3cf21bf0a6d3327cbc01d58fb94f
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-08 11:37:23 -0700

    added gravity library

--- pythonsrc/rod_mechanics/external_forces/gravity_force.py
+++ pythonsrc/rod_mechanics/external_forces/gravity_force.py
@@ -5,5 +5,13 @@ class GravityForce(BaseForce):
     def __init__(self, soft_robots, g_vector):
         super().__init__(soft_robots)
         self.g_vector = g_vector
+        self.setGravity()
 
-    
+    def computeForce(self, dt):
+        limb_idx = 0
+        for limb in super().soft_robots:
+            mass_gravity = mass_gravity[limb_idx]
+
+    def setGravity(self):
+        for limb in super().soft_robots:
+            self.mass_gravity = 0
