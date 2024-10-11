3f12eeb00a4322ba075bcc330af107a658acda8f
Shoheicode <ucodejasonirie@gmail.com>
2024-10-11 12:26:54 -0700

    updated elastic rod

--- pythonFolder/pythonsrc/rod_mechanics/elastic_rod.py
+++ pythonFolder/pythonsrc/rod_mechanics/elastic_rod.py
@@ -349,7 +349,22 @@ class ElasticRod:
     def __get_ref_twist(self):
         pass
     def __compute_kappa(self):
-        pass
+        # First loop: Compute kb using the tangent vectors
+        for i in range(1, self.ne):
+            t0 = self.tangent[i - 1, :]  # Get the (i-1)th row of the tangent array
+            t1 = self.tangent[i, :]      # Get the ith row of the tangent array
+            self.kb[i, :] = 2.0 * np.cross(t0, t1) / (1.0 + np.dot(t0, t1))
+
+        # Second loop: Compute kappa using m1, m2, and kb
+        for i in range(1, self.ne):
+            m1e = self.m1[i - 1, :]  # Get the (i-1)th row of m1
+            m2e = self.m2[i - 1, :]  # Get the (i-1)th row of m2
+            m1f = self.m1[i, :]      # Get the ith row of m1
+            m2f = self.m2[i, :]      # Get the ith row of m2
+
+            # Calculate the values for kappa
+            self.kappa[i, 0] = 0.5 * np.dot(self.kb[i, :], (m2e + m2f))  # First component of kappa
+            self.kappa[i, 1] = -0.5 * np.dot(self.kb[i, :], (m1e + m1f))  # Second component of kappa
 
     @staticmethod
     def __parallel_transport(self, d1_1: np.ndarray, t1: np.ndarray, t2: np.ndarray, 
