f865742c6e718c940d76e53b6dc85ea7af5a3040
Shoheicode <ucodejasonirie@gmail.com>
2024-10-15 14:26:06 -0700

    updated test

--- pythonFolder/test.py
+++ pythonFolder/test.py
@@ -30,6 +30,27 @@ edges = (
     (5,7)
     )
 
+# Function to render the XYZ axes
+def draw_axes():
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+
+    # X-axis (Red)
+    glColor3f(1, 0, 0)  # Red color
+    glVertex3f(0, 0, 0)  # Starting point
+    glVertex3f(1, 0, 0)  # End point
+
+    # Y-axis (Green)
+    glColor3f(0, 1, 0)  # Green color
+    glVertex3f(0, 0, 0)  # Starting point
+    glVertex3f(0, 1, 0)  # End point
+
+    # Z-axis (Blue)
+    glColor3f(0, 0, 1)  # Blue color
+    glVertex3f(0, 0, 0)  # Starting point
+    glVertex3f(0, 0, 1)  # End point
+
+    glEnd()
 
 def Cube():
     glBegin(GL_LINES)
@@ -54,9 +75,10 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        # glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
+        draw_axes()
         pygame.display.flip()
         pygame.time.wait(10)
 
