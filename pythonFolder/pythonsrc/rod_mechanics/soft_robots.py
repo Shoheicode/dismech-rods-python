43eaab543ea0d77514699ace6d7626aebd5580f0
Shoheicode <ucodejasonirie@gmail.com>
2024-10-17 10:23:52 -0700

    updated soft robot

--- pythonFolder/pythonsrc/rod_mechanics/soft_robots.py
+++ pythonFolder/pythonsrc/rod_mechanics/soft_robots.py
@@ -6,27 +6,47 @@ from pythonsrc.rod_mechanics.elasticjoint import ElasticJoint
 
 class SoftRobots():
     def __init__(self):
-        self.limbs :List[ElasticRod] = List[ElasticRod]
-        self.joints :List[ElasticRod] = List[ElasticJoint]
+        self.limbs :List[ElasticRod] = [] # List of elastic rod (limbs)
+        self.joints :List[ElasticJoint] = [] # List of elastic joints
         self.controllers = []
+        self.num_limbs = 0  # Counter to track the number of limbs
 
-    def addLimb(self, start, end, num_nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
-        self.limbs.append("hi")
+    def add_limb(self, start, end, num_nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
+        limb = ElasticRod(self.num_limbs, start, end, num_nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu)
+        self.limbs.append(limb)  # Add new limb to the list
+        self.num_limbs += 1  # Increment limb counter
 
-    def addLimb(nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
-        pass
+    def add_limb_with_nodes(self, nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu = 0.0):
+        limb = ElasticRod(self.num_limbs, nodes, rho, rod_radius, youngs_modulus, poisson_ratio, mu)
+        self.limbs.append(limb)  # Add limb to the list
+        self.num_limbs += 1  # Increment limb counter
 
-    def createJoint(limb_idx, node_idx):
-        pass
+    def create_joint(self, limb_idx, m_node_idx):
+        # Default to the last node if m_node_idx is -1
+        node_idx = m_node_idx if m_node_idx != -1 else self.limbs[limb_idx].nv - 1
+        joint = ElasticJoint(node_idx, limb_idx, self.limbs)
+        self.joints.append(joint)  # Add joint to the list
 
-    def addToJoint(joint_idx, limb_idx, node_idx):
-        pass
+    def add_to_joint(self,joint_idx, limb_idx, m_node_idx):
+        # Default to the last node if m_node_idx is -1
+        node_idx = m_node_idx if m_node_idx != -1 else self.limbs[limb_idx].nv - 1
+        self.joints[joint_idx].add_to_joint(node_idx, limb_idx)  # Add limb to the existing joint
 
-    def lockEdge(limb_idx, edge_idx):
-        pass
+    def lockEdge(self, limb_idx, edge_idx):
+        limb : ElasticRod = self.limbs[limb_idx]
+        # Set boundary conditions on the two vertices of the edge
+        limb.set_vertex_boundary_condition(limb.get_vertex(edge_idx), edge_idx)
+        limb.set_vertex_boundary_condition(limb.get_vertex(edge_idx + 1), edge_idx + 1)
+        # Set boundary condition for twist angle (theta)
+        limb.set_theta_boundary_condition(0.0, edge_idx)
 
-    def applyInitialVelocities(limb_idx, velocities):
-        pass
+    def applyInitialVelocities(self, limb_idx, velocities):
+        limb : ElasticRod = self.limbs[limb_idx]
+        if limb.nv != len(velocities):
+            raise ValueError(f"The number of nodes ({limb.nv}) and velocities ({len(velocities)}) given did not match!")
+        for i in range(limb.nv):
+            # Set initial velocities for the limb's nodes
+            limb.u[4*i:4*i+3] = limb.u0[4*i:4*i+3] = velocities[i]
 
     def setup(self):
         pass
