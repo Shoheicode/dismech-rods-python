2aeebb2758efbd073d717cdfbca0c98c4f4099aa
Shoheicode <ucodejasonirie@gmail.com>
2024-10-18 13:22:29 -0700

    updated elastic rod

--- pythonFolder/pythonsrc/rod_mechanics/elastic_rod.py
+++ pythonFolder/pythonsrc/rod_mechanics/elastic_rod.py
@@ -432,7 +432,7 @@ class ElasticRod:
             t =  self.tangent[i]
             ut = np.zeros(3)
             ut = self.__parallel_transport(u0,t0,t, ut)
-            ut = self.__rotate_axis_angle(ut,t,self.ref_twist[i])
+            ut = self.__rotate_axis_angle(ut,t,self.ref_twist_old[i])
             sgnAngle = self.__signed_angle(ut,u1,t)
             self.ref_twist[i] = self.ref_twist_old[i] + sgnAngle
 
