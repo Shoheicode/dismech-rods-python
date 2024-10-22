ac24973a9ffb970848b459ebbb01f35aaa854ec3
Shoheicode <ucodejasonirie@gmail.com>
2024-10-22 12:17:11 -0700

    updated code

--- pythonFolder/TestPythonCases/TestGetRefTwist.py
+++ pythonFolder/TestPythonCases/TestGetRefTwist.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def get_ref_twist(self):
+        # nv = len(refTwist)
+        # self.ne = self.nv - 1
+        for i in np.arange(1,self.ne):
+            u0 = self.d1[i-1]
+            u1 = self.d1[i]
+            t0 = self.tangent[i-1]
+            t =  self.tangent[i]
+            ut = np.zeros(3)
+            ut = self.__parallel_transport(u0,t0,t, ut)
+            ut = self.__rotate_axis_angle(ut,t,self.ref_twist_old[i])
+            sgnAngle = self.__signed_angle(ut,u1,t)
+            self.ref_twist[i] = self.ref_twist_old[i] + sgnAngle
+
 def test_getRefTwist():
   """
   This function tests the getRefTwist function by comparing the output
