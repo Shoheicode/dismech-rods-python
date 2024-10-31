8d63ae533939f7bd604d04d0d0e3729ac03cbab3
Shoheicode <ucodejasonirie@gmail.com>
2024-10-31 11:37:48 -0700

    updated

--- pythonFolder/pythonsrc/time_stepper/backward_euler.py
+++ pythonFolder/pythonsrc/time_stepper/backward_euler.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pythonsrc.time_stepper.implicit_time_stepper import ImplicitTimeStepper
 from pythonsrc.globalDefinitions import SimParams
 from pythonsrc.rod_mechanics.force_container import ForceContainer
@@ -18,7 +20,63 @@ class BackwardEuler(ImplicitTimeStepper):
 
     def newton_method(self, dt):
         # Override with Newton's method implementation.
-        return 0.0  # Replace with the actual return value from the method's computation
+        normf = 0
+        normf0 = 0
+        solved = False
+        self.iter = 0
+
+        while not solved:
+            self.prep_system_for_iteration()
+            self.forces.compute_forces_and_jacobian(dt)
+
+            # Compute norm of the force equations
+            normf = np.linalg.norm(self.Force)
+
+            if self.iter == 0:
+                normf0 = normf
+
+            # Force tolerance check
+            if normf <= normf0 * self.ftol:
+                solved = True
+                self.iter += 1
+                continue
+
+            # Adaptive time stepping if enabled
+            if self.adaptive_time_stepping and self.iter != 0 and self.iter % self.adaptive_time_stepping_threshold == 0:
+                dt *= 0.5
+                for limb in self.limbs:
+                    limb.update_guess(0.01, dt)
+                self.iter += 1
+                continue
+
+            # Solve equations of motion
+            self.integrator()
+            if self.line_search_enabled:
+                self.line_search(dt)
+
+            # Newton update
+            max_dx = 0
+            for idx, limb in enumerate(self.limbs):
+                curr_dx = limb.update_newton_x(self.dx, self.offsets[idx], self.alpha)
+                max_dx = max(max_dx, curr_dx)
+
+            # Dynamics tolerance check
+            if max_dx / dt < self.dtol:
+                solved = True
+                self.iter += 1
+                continue
+
+            self.iter += 1
+
+            # Exit if unable to converge
+            if self.iter > self.max_iter:
+                if self.terminate_at_max:
+                    print(f"No convergence after {self.max_iter} iterations")
+                    exit(1)
+                else:
+                    solved = True
+
+        return dt
 
     def line_search(self, dt):
         # Override with line search implementation.
