22b670d12440a8405bd7d4dca413fb1e3888d999
Shoheicode <ucodejasonirie@gmail.com>
2024-11-26 11:58:41 -0800

    update

--- pythonFolder/pythonsrc/time_stepper/implicit_midpoint.py
+++ pythonFolder/pythonsrc/time_stepper/implicit_midpoint.py
@@ -41,33 +41,17 @@ class ImplicitMidpoint(BackwardEuler):
         # print("DT VALUE: ", self.dt)
 
         for limb in self.limbs:
-            # Compute velocity at T = t + 0.5 * dt
-            # print("PRINT LIMBX", limb.x)
-            # print("PRINT LIMB BEFORE", limb.x0)
             limb.u = (limb.x - limb.x0) / (0.5 * self.dt)
 
-            # print("U POSITION", limb.u)
-
-            # Compute position at T = t + dt
             limb.x = 2 * limb.x - limb.x0
-            # for i in range(limb.ne):
-            #     # print("X POSITION", i, ":", limb.x[i*4])
-            #     # print("Y POSITION", i, ":", limb.x[i*4+1])
-            #     print("Z POSITION", i, ":", limb.x[i*4+2])
+
             limb.x0 = limb.x.copy()
 
-            # print("LIMB VALUE X0", limb.x)
-
-            # Compute velocity at T = t + dt
             limb.u = 2 * limb.u - limb.u0
             
             limb.u0 = limb.u.copy()
 
-            # print("U POSITION2: ", limb.u0)
-
         # Update the system for the next time step
         self.update_system_for_next_time_step()
-        # for limb in self.limbs:
-        #     print("LIMB BALUE", limb.x0)
 
         return self.dt
